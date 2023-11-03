47afe7e79c7cac56fbc2e930f9a889c89e47e111
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-11-03 16:02:56 +0100

    first version of html ,css ,js (2)

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+  let amenityIds = {};
+  $('input[type="checkbox"]').change(function() {
+    if (this.checked) {
+      amenityIds[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenityIds[$(this).data('id')];
+    }
+    let amenities = Object.values(amenityIds);
+    if (amenities.length === 0) {
+      $('.amenities h4').html(' ');
+    } else {
+      $('.amenities h4').text(amenities.join(', '));
+    }
+  });
+});
